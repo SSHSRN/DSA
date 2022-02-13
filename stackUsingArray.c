99a2a033e131be7224bf787ff837aff52711a9b9
SSHSRN <62781914+SSHSRN@users.noreply.github.com>
2022-02-13 18:28:41 +0530

    added push operation

--- stackUsingArray.c
+++ stackUsingArray.c
@@ -25,7 +25,34 @@ int isFull(struct stack * ptr){
     }
 }
 
+void traverse(struct stack * ptr){
+    for(int i = 0; i <= ptr -> top; i++){
+        printf("%d\n", ptr -> arr[i]);
+    }
+}
+
+void push(struct stack * ptr, int val){
+    if(isFull(ptr)){
+        printf("Stack Overflow! Cannot push %d to the stack.\n", val);
+    }
+    else{
+        ptr -> top++;
+        ptr -> arr[ptr -> top] = val;
+    }
+}
+
+int pop(struct stack * ptr){
+    if(isEmpty(ptr)){
+        printf("Stack UnderFlow!\n");
+    }
+    else{
+        ptr -> top--;
+    }
+    return ptr -> top;
+}
+
 int main(){
+    int choice, val;
     // struct stack s;
     // // Size of Stack is 50.
     // s.size = 50;
@@ -35,7 +62,7 @@ int main(){
     // s.arr = (int *) malloc (s.size * sizeof(int));
 
     // Instead of s, the declaration can also be done with a pointer(*s).
-    struct stack *s1;
+    struct stack *s1 = (struct stack *) malloc (sizeof(struct stack));
     // Size of Stack is 50.
     s1 -> size = 50;
     // Top is -1 as the Stack is empty.
@@ -43,24 +70,48 @@ int main(){
     // Assigning a memory space for the array in heap.
     s1 -> arr = (int *) malloc (s1 -> size * sizeof(int));
 
-    // Pushing an element manually.
-    // s1 -> arr[0] = 5;
-    // s1 -> top++;
+    // Pushing a few element manually.
+    s1 -> arr[0] = 5;
+    s1 -> top++;
+    s1 -> arr[1] = 10;
+    s1 -> top++;
+    s1 -> arr[2] = 15;
+    s1 -> top++;
+    s1 -> arr[3] = 20;
+    s1 -> top++;
+    s1 -> arr[4] = 25;
+    s1 -> top++;
+    
+    printf("Stack operations.\nEnter 0 to Traverse the Stack.\nEnter 1 to Push an element.\nEnter 2 to Pop an element.\nEnter your choice: ");
+    scanf("%d",&choice);
+    if(choice ==0){
+        printf("\nTraversal\n\n");
+        traverse(s1);
+    }
 
-    // Check if stack is empty.
-    if(isEmpty(s1)){
-        printf("\nStack is Empty!\n\n");
-    }
-    else{
-        printf("\nStack is not Empty!\n\n");
+    else if(choice == 1){
+        printf("\nInsertion\n\nEnter the value to insert:");
+        scanf("%d", &val);
+        push(s1, val);
+        traverse(s1);
     }
 
-    // Check if stack is full.
-    if(isFull(s1)){
-        printf("\nStack is Full!\n\n");
-    }
-    else{
-        printf("\nStack is not Full!\n\n");
-    }
+    // // Check if stack is empty.
+    // if(isEmpty(s1)){
+    //     printf("\nStack is Empty!\n\n");
+    // }
+    // else{
+    //     printf("\nStack is not Empty!\n\n");
+    // }
+
+    // // Check if stack is full.
+    // if(isFull(s1)){
+    //     printf("\nStack is Full!\n\n");
+    // }
+    // else{
+    //     printf("\nStack is not Full!\n\n");
+    // }
     return 0;
 }
+
+// Watched till 14.32
