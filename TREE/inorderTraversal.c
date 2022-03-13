7219b2b943d29a32aa6d609c723640858fb7772c
SSHSRN <62781914+SSHSRN@users.noreply.github.com>
2022-03-13 21:58:33 +0530

    added inorder traversal

---
+++ TREE/inorderTraversal.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <malloc.h>
+
+struct node{
+    int data;
+    struct node * left;
+    struct node * right;
+};
+
+struct node * createNode(int value){
+    struct node * p; // Creating a node pointer
+    p = (struct node *) malloc (sizeof(struct node)); // Allocating memory in the heap
+    p -> data = value; // Setting the data of the node
+    p -> left = NULL; // Setting the left child to NULL
+    p -> right = NULL; // Setting the right child to NULL
+    return p; // Returning the node
+}
+
+void inorderTraverse(struct node * root){
+    if(root != NULL){
+        inorderTraverse(root -> left);
+        printf("%d\n", root -> data);
+        inorderTraverse(root -> right);
+    }
+}
+
+int main(){
+    // Constructing the root node using function
+    struct node * p = createNode(5);
+
+    struct node * p1 = createNode(10);
+    struct node * p2 = createNode(15);
+
+    // Linking the root node with left and right children.
+    p -> left = p1;
+    p -> right = p2;
+
+    // Calling the preorder traversal function
+    inorderTraverse(p);
+    return 0;
+}
